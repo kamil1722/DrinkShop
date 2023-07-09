11d02688c29a48ba17adf7b442c91b7a9de8c98e
Kamil <kamilaliev1722@gmail.com>
2023-07-09 18:13:47 +0300

    Подключени к бд + отображение, редактирование данных через веб страницу

---
+++ VVM/Data/VVMContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VVM.Models;
+
+namespace VVM.Data
+{
+    public class VVMContext : DbContext
+    {
+        public VVMContext (DbContextOptions<VVMContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VVM.Models.Drinks> Drinks { get; set; } = default!;
+    }
+}
---
+++ VVM/Models/Drinks.cs
@@ -0,0 +1,11 @@
+﻿namespace VVM.Models
+{
+	public class Drinks
+	{
+		public Guid id { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public int count { get; set; }
+		public int price { get; set; }
+	}
+}
