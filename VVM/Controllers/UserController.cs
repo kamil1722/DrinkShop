ab54867ddb8f5ba8f691ba04c75956f5af049887
Kamil <kamilaliev1722@gmail.com>
2023-08-06 16:54:35 +0300

    добавление ajax запроса для обновления количества напитков

--- VVM/Controllers/UserController.cs
+++ VVM/Controllers/UserController.cs
@@ -17,5 +17,24 @@ namespace VVM.Controllers
         {
             return View(await _context.Drinks.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCount(int codeDrink)
+        {
+            if (_context.Drinks == null)
+            {
+                return Problem("Entity set 'VVMContext.Drinks' is null.");
+            }
+            var drink = await _context.Drinks.FindAsync(codeDrink);
+            if (drink != null)
+            {
+                drink.Count--;
+                _context.Drinks.Update(drink);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- VVM/Program.cs
+++ VVM/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Admin}/{action=Index}/{id?}");
+	pattern: "{controller=User}/{action=Index}/{id?}");
 
 app.Run();
